83b136434bc58ebbc11a6836f7f8086584505e57
David Pokorák <david.pokorak021@skolavdf.cz>
2024-04-28 20:58:33 +0200

    clean up
    Deleted parts that aint working

--- bub/www/js/script.js
+++ bub/www/js/script.js
@@ -6,17 +6,9 @@ function load(url, element) {
 			element.innerHTML = data;
 		});
 }
-function load_recipe(url) {
-	fetch(url)
-		.then((response) => response.text())
-		.then((data) => {
-			const newTab = window.open();
-			newTab.document.write(data);
-		});
-}
 
 function sendRequest(id) {
-	load_recipe(api_url + "/recipe.php?id=" + encodeURIComponent(id));
+	window.open(api_url + "recipe.php?id=" + encodeURIComponent(id));
 }
 
 load(api_url + "recipes.php", document.getElementById("recipeList"));
